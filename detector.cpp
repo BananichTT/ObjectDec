bf70c56eb9a7192b687d4eae14da0c823ecd012e
Buldin Alexandr <buldin.alieksandr@gmail.com>
2022-05-28 21:44:31 +0300

    add function for reading from file

--- detector.cpp
+++ detector.cpp
@@ -1,9 +1,11 @@
 #include <detector.h>
 #include <iostream>
+#include <QFile>
+
 detector::detector(int a)
 {
-    cv::Mat img = cv::imread("D:/img.jpg");
-    cv::Mat templ = cv::imread("D:/img1.jpg");
+    cv::Mat img = cv::imread("D:/borig.jpg");
+    cv::Mat templ = cv::imread("D:/b2.jpg");
     std::cout<<"dsaghjh";
     add_template(templ);
     input_data(img, methods::CCOEFF_NORMED);
@@ -22,51 +24,76 @@ void detector::input_data(const std::vector<uint8_t> &buffer, methods m)
     det_img(img, m);
 }
 
-std::pair<int, state> detector::parse_data(const std::string &file, methods m)
+cv::Mat Decode(std::vector<uint8_t> buff, int camera_id)
 {
+    cv::Mat img = imdecode(cv::Mat(buff), cv::IMREAD_UNCHANGED);
+    return img;
 
 }
 
-std::pair<int, state> detector::add_template(const cv::Mat &templ_one)
+std::pair<int, state> detector::parse_data(const std::string &m_fileName, methods m, frame_state state, image_saving_protocol &read_protocol)
 {
-    cv::Mat tmp = templ_one;
-    templ_counter ++;
+    std::ifstream fs(m_fileName, std::ios::binary);
+    switch(state)
+    {
+        case frame_state::next:{position += 2; break;}
+        case frame_state::previos:{position -= 2; break;}
+        default: {position += 0;}
+    }
+    int f = 0;
+    fs.seekg(FrameByteIndex[position], std::ios_base::beg);
+    while (f < 2)
+    {
+        int fff = 0;
+        fs.read((char*)&fff,sizeof(unsigned int));
+        fs.read((char*)&read_protocol.NUMBER_OF_FRAMES,sizeof (unsigned int));
+        fs.read((char*)&read_protocol.tmsec,sizeof (uint64));
+        int size;
+        fs.read((char*)&size,sizeof(int));
+        std::vector<uint8_t> buff1;
+        buff1.resize(size);
+        fs.read(reinterpret_cast<char*>(&buff1.front()),buff1.size());
+        det_img(Decode(buff1,fff),m);
+        f++;
+    }
+}
+
+std::pair<int, state> detector::add_template(const cv::Mat &templ_one) {
+  cv::Mat tmp = templ_one;
+  templ_counter++;
+  templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
+
+  return std::pair<int, state>(templ_counter, state::success);
+}
+
+std::pair<std::vector<int>, state> detector::add_template(std::vector<cv::Mat> &templs) {
+  std::vector<int> position; // возвращает позицию изображения
+  for (const auto &img : templs) {
+    cv::Mat tmp = img;
+    templ_counter++;
     templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
-
-return std::pair<int, state>(templ_counter, state::success);
+    position.push_back(templ_counter);
+  }
+  return std::pair<std::vector<int>, state>(position, state::success);
 }
 
-std::pair<std::vector<int>, state> detector::add_template(std::vector<cv::Mat> &templs)
-{
-    std::vector<int> position; // возвращает позицию изображения
-    for (const auto &img:templs){
-        cv::Mat tmp = img;
-        templ_counter ++;
-        templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
-        position.push_back(templ_counter);
-    }
-    return std::pair<std::vector<int>, state>(position, state::success);
+std::pair<int, state> detector::add_template(std::string &templ_one) {
+  cv::Mat tmp = cv::imread(templ_one);
+  templ_counter++;
+  templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
+
+  return std::pair<int, state>(templ_counter, state::success);
 }
 
-std::pair<int, state> detector::add_template(std::string &templ_one)
-{
-        cv::Mat tmp = cv::imread(templ_one);
-        templ_counter ++;
-        templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
-
-    return std::pair<int, state>(templ_counter, state::success);
-}
-
-std::pair<std::vector<int>, state> detector::add_template(std::vector<std::string> &templs)
-{
-    std::vector<int> position; // возвращает позицию изображения
-    for (const auto &file_name:templs){
-        cv::Mat tmp = cv::imread(file_name);
-        templ_counter ++;
-        templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
-        position.push_back(templ_counter);
-    }
-    return std::pair<std::vector<int>, state>(position, state::success);
+std::pair<std::vector<int>, state> detector::add_template(std::vector<std::string> &templs) {
+  std::vector<int> position; // возвращает позицию изображения
+  for (const auto &file_name : templs) {
+    cv::Mat tmp = cv::imread(file_name);
+    templ_counter++;
+    templ.insert(std::pair<int, cv::Mat>(templ_counter, tmp));
+    position.push_back(templ_counter);
+  }
+  return std::pair<std::vector<int>, state>(position, state::success);
 }
 
 QVector<cv::Point> tresh(double treshold, const cv::Mat &result) {
@@ -123,32 +150,6 @@ void draw_rect(cv::Mat &qimg, const QVector<QVector<cv::Point>> point_mas){
     }
 };
 
-//void find_conturs(cv::Mat &input_img){
-//    //adaptiveThreshold(input_img, input_img, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 101,
-//                      //15);
-//    cvtColor(input_img, input_img, cv::COLOR_BGR2GRAY);
-//    cv::Mat canny_output;
-//    cv::Canny(input_img, canny_output, 100, 100*2 );
-//    std::vector<std::vector<cv::Point> > contours;
-//    std::vector<cv::Vec4i> hierarchy;
-//    cv::findContours( canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE );
-
-//    std::vector<std::vector<cv::Point>> hull(contours.size());
-//    for(unsigned int i = 0,n = contours.size(); i < n; ++i) {
-//        cv::convexHull(cv::Mat(contours[i]),hull[i],false);
-//    }
-
-//    cv::Mat contours_result = cv::Mat::zeros(input_img.size(), CV_8UC3);
-
-//    for (unsigned int i = 0, n = contours.size(); i < n; ++i)
-//    {
-//      cv::Scalar color = cv::Scalar(0,0,255);
-//      cv::drawContours(input_img, contours, i,color, 4, 8, hierarchy,0, cv::Point());
-//    }
-
-// cv::imshow("Contours Result",input_img);
-// cv::waitKey(0);
-//};
 
 void detector::det_img(const cv::Mat &image, methods m)
 {
@@ -183,5 +184,33 @@ void detector::det_img(const cv::Mat &image, methods m)
     cv::imshow("csda", img);
 
     cv::waitKey(0);
+}
+
+std::vector<uint64> detector::Data_Indexing(const QString &m_fileName)
+{
+    std::vector<uint64> indexes;
+    std::ifstream fs(m_fileName.toStdString(), std::ios::binary);
+    QFile file(m_fileName);
+    if(file.exists())
+    {
+        image_saving_protocol read_protocol;
+        uint64 def_size = 0;
+        indexes.push_back(def_size);
+        while(!fs.eof())
+        {
+            fs.read((char*)&read_protocol.CAMERA_ID,sizeof(unsigned int));
+            fs.read((char*)&read_protocol.NUMBER_OF_FRAMES,sizeof (unsigned int));
+            fs.read((char*)&read_protocol.tmsec,sizeof (uint64));
+            int size;
+            fs.read((char*)&size,sizeof(int));
+            std::vector<uint8_t> buff1;
+            buff1.resize(size);
+            fs.read(reinterpret_cast<char*>(&buff1.front()),buff1.size());
+            def_size += size;
+            def_size += 20;
+            indexes.push_back(def_size);
+        }
+    }
+    return indexes;
 }
 
--- detector.h
+++ detector.h
@@ -9,8 +9,10 @@
 #include <QMap>
 #include <QVector>
 #include <QDebug>
+#include <fstream>
 enum state{success, error};
 enum methods{SQDIFF = 0, SQDIFF_NORMED = 1, CCORR = 2, CORR_NORMED = 3, CCOEFF = 4, CCOEFF_NORMED = 5};
+enum frame_state {next,previos};
 
 // информация о найденых объектах
 struct object_inf {
@@ -19,13 +21,22 @@ struct object_inf {
     cv::Mat templateMatching_result;
 };
 
+struct image_saving_protocol
+{
+    unsigned int CAMERA_ID;
+    unsigned int NUMBER_OF_FRAMES;
+    uint64 tmsec;
+    cv::Mat frame;
+    std::vector<uint8_t> imgbuff;
+};
+
 class detector:public QObject{
     Q_OBJECT
 public :
     detector(int a);
     void input_data(const cv::Mat &image, methods m); // подача на вход изображения
     void input_data(const std::vector<uint8_t> &buffer, methods m); // подача изображения в другой форме
-    std::pair<int, state> parse_data(const std::string &file, methods m); // путь до файла с видео
+     std::pair<int, state> parse_data(const std::string &m_fileName, methods m, frame_state state, image_saving_protocol &read_protocol); // путь до файла с видео
     std::pair<int, state> add_template(const cv::Mat &templ_one); // загрузка шаблона
     std::pair<std::vector<int>, state> add_template(std::vector<cv::Mat> &templs); // загрузка нескольких шаблонов
     std::pair<int, state> add_template(std::string &templ_one); // закгрузка пути до файла шаблона
@@ -33,19 +44,19 @@ public :
 
     //  std::pair<int, state> - возвращает номер шаблона в хранлище и состояние загрузки, если error - номер шаблона -1
 
-private :
+  private:
     std::map<int, cv::Mat> templ;
+    std::vector<uint64> FrameByteIndex;
     int templ_counter = 0;
+    int m_b = qRegisterMetaType<image_saving_protocol>("image_saving_protocol");
+    int position = 0;
 
-private  :
+  private:
     void det_img(const cv::Mat &image, methods m);
+    std::vector<uint64> Data_Indexing(const QString &m_fileName);
 
-signals :
+  signals:
     void output_data(object_inf info);
-
 };
-
-
-
 
 #endif
