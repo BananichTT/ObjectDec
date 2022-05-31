d6e83fa3e7667eb23af393b876ee8ca791ee98b7
Buldin Alexandr <buldin.alieksandr@gmail.com>
2022-05-31 11:51:22 +0300

    fixed drawing of squares

--- detector.cpp
+++ detector.cpp
@@ -1,6 +1,8 @@
 #include <detector.h>
 #include <iostream>
 #include <QFile>
+#define width 50
+#define height 50
 
 detector::detector(int a)
 {
@@ -15,7 +17,8 @@ void detector::input_data(const cv::Mat &image, methods m)
 {
     cv::Mat img = image;
     det_img(image, m);
-    std::cout<<"fdsshs";
+
+
 }
 
 void detector::input_data(const std::vector<uint8_t> &buffer, methods m)
@@ -106,7 +109,7 @@ QVector<cv::Point> tresh(double treshold, const cv::Mat &result) {
     for (int j = 0; j < result1.cols; j++) {
 
       //qDebug()<<result.at<float>(i,j);
-      uchar a = static_cast<uchar>((result.at<float>(i,j) * 127) +255);
+      uchar a = static_cast<uchar>((result.at<float>(i,j) * 127) -255);
       test.at<uchar>(i,j) = a;
       if (result.at<float>(i, j) > treshold) {
 
@@ -116,74 +119,126 @@ QVector<cv::Point> tresh(double treshold, const cv::Mat &result) {
   }
   adaptiveThreshold(test, test, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 101,
                     15);
-  cv::imshow("test", test);
-  cv::waitKey(0);
   return vec;
 }
 
-void delete_point(QVector<cv::Point> &q){
+//void delete_point(QVector<cv::Point> &q){
 
-    QVector<cv::Point>::Iterator iterator;
-    QVector<cv::Point>::Iterator iterator2;
+//    QVector<cv::Point>::Iterator iterator;
+//    QVector<cv::Point>::Iterator iterator2;
 
-    for (iterator = q.begin(); iterator != q.end(); iterator++) {
-      for (iterator2 = iterator + 1; iterator2 != q.end();) {
-        double dist =
-            sqrt((iterator->x - iterator2->x) * (iterator->x - iterator2->x) +
-                 (iterator->y - iterator2->y) * (iterator->y - iterator2->y));
-        if (dist < 25) {
-          iterator2 = q.erase(iterator2);
-        } else {
-          iterator2++;
-        }
-      }
-    }
-};
+//    for (iterator = q.begin(); iterator != q.end(); iterator++) {
+//      for (iterator2 = iterator + 1; iterator2 != q.end();) {
+//        double dist =
+//            sqrt((iterator->x - iterator2->x) * (iterator->x - iterator2->x) +
+//                 (iterator->y - iterator2->y) * (iterator->y - iterator2->y));
+//        if (dist < 25) {
+//          iterator2 = q.erase(iterator2);
+//        } else {
+//          iterator2++;
+//        }
+//      }
+//    }
+//}
 
 void draw_rect(cv::Mat &qimg, const QVector<QVector<cv::Point>> point_mas){
     for(int i = 0; i < point_mas.length(); i++){
         for(int j = 0; j < point_mas[i].length(); j++){
             rectangle(qimg, cv::Point(point_mas[i][j].x, point_mas[i][j].y),
-                      cv::Point(point_mas[i][j].x + 50, point_mas[i][j].y + 50), cv::Scalar(0, 255, 0),
+                      cv::Point(point_mas[i][j].x + /*50*/75, point_mas[i][j].y + /*50*/90), cv::Scalar(/*255, 255, 255*/0,0,0),
                       2);
+            //cv::circle(qimg, cv::Point(point_mas[i][j].x + 35, point_mas[i][j].y + 45), 5, cv::Scalar(255,255,255), 2);
+
         }
     }
+}
+
+cv::Mat convert_to_CV_8UC1(const cv::Mat &result) {
+  cv::Mat_<uchar> result1 = result;
+  cv::Mat test(result.size(), CV_8UC1);
+  for (int i = 0; i < result1.rows; i++) {
+    for (int j = 0; j < result1.cols; j++) {
+      uchar a = static_cast<uchar>((result.at<float>(i, j) * 127) + 255);
+      test.at<uchar>(i, j) = a;
+    }
+  }
+  cv::imshow("test", test);
+  cv::waitKey(0);
+  return test;
 };
 
+QVector<cv::Point> find_conturs(cv::Mat &input_img){
+    adaptiveThreshold(input_img, input_img, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 101,
+                      15);
+
+    cv::Mat canny_output;
+    cv::Canny(input_img, canny_output, 100, 100*2 );
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours( canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE );
+
+    std::vector<std::vector<cv::Point>> hull(contours.size());
+    for(unsigned int i = 0,n = contours.size(); i < n; ++i) {
+        cv::convexHull(cv::Mat(contours[i]),hull[i],false);
+    }
+    cv::Scalar color = cv::Scalar(255,255,255);
+    cv::Mat contours_result = cv::Mat::zeros(input_img.size(), CV_8UC3);
+
+    QVector<cv::Point> point;
+
+    for (unsigned int i = 0, n = contours.size(); i < n; ++i) {
+        if(contours[i].size() > 5){
+            cv::RotatedRect rect = cv::fitEllipse(cv::Mat(contours[i]));
+            point.push_back(rect.center);
+        }
+
+    }
+
+    return point;
+};
 
 void detector::det_img(const cv::Mat &image, methods m)
 {
-    QVector<QVector<cv::Point>> point_mas;
+  QVector<QVector<cv::Point>> point_mas;
 
-    cv::Mat img = image;
-    int match_method = m;
-    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
-    adaptiveThreshold(img, img, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 101,
-                      15);
+  cv::Mat img = image;
+  int match_method = m;
+  cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
 
+  adaptiveThreshold(img, img, 255, cv::ADAPTIVE_THRESH_MEAN_C,
+                    cv::THRESH_BINARY, 101, 15);
+ /* cv::bitwise_not(img, img);*/ // инвентирование НЕГАТИВ
+  cv::imshow("fsghs",img);
+  cv::waitKey(0);
 
-    std::map<int, cv::Mat>::iterator iterator;
+  std::map<int, cv::Mat>::iterator iterator;
 
-    for(iterator = templ.begin(); iterator != templ.end(); iterator++){
-        cvtColor(iterator->second, iterator->second, cv::COLOR_BGR2GRAY);
-        adaptiveThreshold(iterator->second, iterator->second, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 101, 15);
+  for (iterator = templ.begin(); iterator != templ.end(); iterator++) {
+    cvtColor(iterator->second, iterator->second, cv::COLOR_BGR2GRAY);
 
-        cv::Mat res_32f(img.rows - iterator->second.rows + 1, img.cols - iterator->second.cols + 1, CV_8UC1);
-        matchTemplate(img, iterator->second, res_32f, match_method);
+//    adaptiveThreshold(iterator->second, iterator->second, 255,
+//                      cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 101, 15);
 
+    cv::Mat res_32f(img.rows - iterator->second.rows + 1,
+                    img.cols - iterator->second.cols + 1, 0);
 
-
-        //find_conturs(res_32f);
-        QVector<cv::Point> q = tresh(0.4, res_32f);
-        delete_point(q);
-        point_mas.push_back(q);
-    }
-
-    draw_rect(img, point_mas);
-    std::cout << "csadca";
-    cv::imshow("csda", img);
-
+    cv::imshow("trmpl",iterator->second);
     cv::waitKey(0);
+    matchTemplate(img, iterator->second, res_32f, match_method);
+
+cv::imshow("res",res_32f);
+cv::waitKey(0);
+
+    cv::Mat convert = convert_to_CV_8UC1(res_32f);
+
+    point_mas.append(find_conturs(convert));
+
+  }
+
+  draw_rect(img, point_mas);
+  std::cout << "csadca";
+  cv::imshow("img", img);
+  cv::waitKey(0);
 }
 
 std::vector<uint64> detector::Data_Indexing(const QString &m_fileName)
